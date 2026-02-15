1585818ad3a4732c402ddb26dcca2218d0d28548
Jude Partovi <jpartovi@college.harvard.edu>
2026-02-15 00:32:16 -0800

    mystery

--- convex/_generated/api.ts
+++
@@ -1,7 +0,0 @@
-/* Stub: Run `npx convex dev` to generate the real API */
-export const api = {
-  scraps: {
-    generateUploadUrl: "scraps:generateUploadUrl" as any,
-    createScrap: "scraps:createScrap" as any,
-  },
-};
